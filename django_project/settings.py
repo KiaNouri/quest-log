3a565ec3420d88f3fa0a85ecc5ac7d245749455a
Kia <prokianouri02@gmail.com>
2026-07-29 19:43:44 +0330

    Add custom user -Add accounts app -Custom user model -Custom admin model -Custom user forms -tests for user and superuser creation

--- django_project/settings.py
+++ django_project/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Local
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# User
+AUTH_USER_MODEL = "accounts.CustomUser"

2d4aa31bac706c25908657b21b41f5c310420171
Kia <prokianouri02@gmail.com>
2026-08-09 18:09:57 +0330

    Add tests for profile model

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 from .models import Profile
 
 
+# create a profile for new users
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_profile(sender, instance, created, **kwargs):
     if created:

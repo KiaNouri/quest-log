2a688b5480baf7d1b4ee91377cd6c2cb17fdfc05
Kia <prokianouri02@gmail.com>
2026-08-10 21:37:12 +0330

    Add Game and Genre models and register to admin + migration

--- apps/games/models.py
+++ apps/games/models.py
@@ -1,3 +1,54 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.text import slugify
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            slug = base_slug
+            n = 1
+            while Genre.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                n += 1
+                slug = f"{base_slug}-{n}"
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    cover_image_url = models.URLField(blank=True)
+    external_id = models.CharField(max_length=100, blank=True)
+    genres = models.ManyToManyField(Genre, blank=True, related_name="games")
+    developer = models.CharField(max_length=255, blank=True)
+    publisher = models.CharField(max_length=255, blank=True)
+    published_year = models.PositiveSmallIntegerField(
+        null=True,
+        blank=True,
+        validators=[
+            MinValueValidator(1970),
+            MaxValueValidator(datetime.date.today().year + 1),  # noqa
+        ],  # +1 for next year releases
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            while Game.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base_slug}-{self.published_year}"
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title

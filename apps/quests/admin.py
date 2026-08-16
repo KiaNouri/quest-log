0429d31fcd91d831334032373f0fd681f9dd1638
Kia <prokianouri02@gmail.com>
2026-08-16 16:34:45 +0330

    Add models + migrations and register them to admin

--- apps/quests/admin.py
+++ apps/quests/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.quests.models import Challenge, Quest, QuestChallenge
+
+
+class QuestChallengeInline(admin.TabularInline):
+    model = QuestChallenge
+    extra = 1
+
+
+@admin.register(Challenge)
+class ChallengeAdmin(admin.ModelAdmin):
+    list_display = ("name", "difficulty", "xp_value")
+    list_filter = ("difficulty",)
+    search_fields = ("name",)
+
+
+@admin.register(Quest)
+class QuestAdmin(admin.ModelAdmin):
+    list_display = ("user", "game", "status", "created_at", "completed_at")
+    list_filter = ("status",)
+    search_fields = ("user__username", "game__title")
+    inlines = (QuestChallengeInline,)
